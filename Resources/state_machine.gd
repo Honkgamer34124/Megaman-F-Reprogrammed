b0a1f5048fce45ec0edae1c102620c81fc23cc35
Kofi K <kofi.k.otchere@gmail.com>
2025-07-11 10:28:57 +0000

    Implement Double State Engine,TODO:Add attack states
    Upgraged State Engine to Double State Engine to use both movement
    and attack states
    Working on Attack States to allow for different attacks and animations.

--- Resources/state_machine.gd
+++ Resources/state_machine.gd
@@ -1,43 +1,63 @@
 extends Node
-@export var initial_state: State
-var current_state: State
-#var new_incoming_state:State
+@export var initial_movement_state: State
+@export var initial_attack_state: State
+var current_movement_state: State
+var current_attack_state: State
 var states: Dictionary = {}
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for child in get_children():
-		if child is State:  #if child is a State,add to dictionary with its key being its name.
+		if child is State:  # if child is a State, add to dictionary with its key being its name.
 			states[child.name.to_lower()] = child
 			child.transition.connect(transitioning_states)
-	if initial_state:
-		initial_state.Enter()
-		current_state = initial_state
-		print("current state:", current_state)
+	if initial_movement_state:
+		initial_movement_state.Enter()
+		current_movement_state = initial_movement_state
+		print("current movement state:", current_movement_state)
+	if initial_attack_state:
+		initial_attack_state.Enter()
+		current_attack_state = initial_attack_state
+		print("Current attack state", current_attack_state)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if current_state:  #if state==current_state,update it
-		current_state.Update(delta)
+	if current_movement_state:  # update movement state
+		current_movement_state.Update(delta)
+	if current_attack_state:  # update attack state
+		current_attack_state.Update(delta)
 
 
 func _physics_process(delta):
-	if current_state:
-		current_state.Physics_Update(delta)
-		#print(name, "current_state:", current_state)
+	if current_movement_state:
+		current_movement_state.Physics_Update(delta)
+	if current_attack_state:
+		current_attack_state.Physics_Update(delta)
+	print(
+		name,
+		":Current movement state,Current attack state:",
+		[current_movement_state, current_attack_state]
+	)
 
 
 func transitioning_states(state, new_incoming_state):
-	if state != current_state:  #checks that state entered==current state
-		return
-
-	var new_state = states.get(new_incoming_state.to_lower())
-	if !new_state:  #checks if the new state we are transitioning to is in the states dictionary/not
-		return
-	if current_state:
-		current_state.Exit()
-	new_state.Enter()
-	current_state = new_state
-	print(name, ":Current state:", current_state)
+	if state == current_movement_state:
+		var new_state = states.get(new_incoming_state.to_lower())
+		if !new_state:
+			return
+		if current_movement_state:
+			current_movement_state.Exit()
+		new_state.Enter()
+		current_movement_state = new_state
+		print(name, ":Current movement state:", current_movement_state)
+	elif state == current_attack_state:
+		var new_state = states.get(new_incoming_state.to_lower())
+		if !new_state:
+			return
+		if current_attack_state:
+			current_attack_state.Exit()
+		new_state.Enter()
+		current_attack_state = new_state
+		print(name, ":Current attack state:", current_attack_state)
---
+++ assets/characters/player/megaman/states/shooting_state.gd
@@ -0,0 +1,58 @@
+extends State
+
+@export var shooting_timer: Timer
+var player
+
+
+func _ready():
+	player = Player.playerCharacter
+	shooting_timer = Timer.new()
+	shooting_timer.one_shot = true
+	shooting_timer.wait_time = 0.5  # duration of shooting animation
+	add_child(shooting_timer)
+	shooting_timer.connect("timeout", _on_shooting_finished)
+
+
+func Enter():
+	if player:
+		#player.animatedSprite2D.play("stand_and_shoot")
+		shooting_timer.start()
+		# Instantiate projectile and play sound
+
+
+func Exit():
+	if shooting_timer.is_stopped() == false:
+		shooting_timer.stop()
+
+
+func Update(_delta: float):
+	pass
+
+
+func Physics_Update(_delta: float):
+	if Input.is_action_just_pressed("shoot"):
+		_spawn_projectile()
+		#print("yeah...")
+
+
+func _on_shooting_finished():
+	#emit_signal("transition", "attack_idle_state")
+	pass  # transition to attack idle or no attack state
+
+
+func _spawn_projectile():
+	if player:
+		print("spawn proj")
+		var bullet = preload("res://weapons/bullet.tscn")
+		var projectile = bullet.instantiate()
+		get_parent().add_child(projectile)
+		if player.animatedSprite2D.flip_h:
+			projectile.direction = "left"
+			projectile.global_position = (
+				player.get_node("shoot_positions/shoot_ground/left").global_position
+			)
+		else:
+			projectile.direction = "right"
+			projectile.global_position = (
+				player.get_node("shoot_positions/shoot_ground/right").global_position
+			)
